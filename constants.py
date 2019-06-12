2eec43e7d30f088784352bf60e6a6783316cc011
sushrut <kasturesushrut@gmail.com>
2019-06-12 23:01:23 +0530

    trying ultrasonic

--- constants.py
+++ constants.py
@@ -1,18 +1,20 @@
 HANDSHAKE_START_HZ = 10000
 HANDSHAKE_END_HZ = 10500
-START_HZ = 1000
+START_HZ = 15000
+STEP_HZ = 30
+
 BITS = 4
+MSGLEN = 8 #message of MSGLEN characters will be sent in one go
+
 START_MSG = "{@}"
 END_MSG = "{|}"
-SAMPLING_RATE = 44100
+
+SAMPLING_RATE = 50000
 maxVol=2**15-1 #maximum amplitude
 FREQ_DURATION = 0.1 #Duration of one frquency in the audio
 
-BASE_FREQ = 1000
-STEP_HZ = 30
-HS_START = 10000
-HS_STOP = 10500
-MSGLEN = 8 #message of MSGLEN characters will be sent in one go
-START_MSG = "{@}"
-END_MSG = "{|}"
+BASE_FREQ = START_HZ
+HS_START = HANDSHAKE_START_HZ
+HS_STOP = HANDSHAKE_END_HZ
+
 
